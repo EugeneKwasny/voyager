a8ba9c805582fd5647b337166d9a717b8ab43bc7
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-02-26 20:09:52 +0200

    Add PopularSearchKeywords component markup

--- components/PopularSearchKeywords.tsx
+++ components/PopularSearchKeywords.tsx
@@ -1,6 +1,31 @@
 export default function PopularSearchKeywords()
 {
     return (
-        <section></section>
+        <section>
+            <div>
+                <a href="#">Wireless Networks</a>
+            </div>
+            <div>
+                <a href="#">Cell Phones</a>
+            </div>
+            <div>
+                <a href="#">Cameras</a>
+            </div>
+            <div>
+                <a href="#">Head Phones</a>
+            </div>
+            <div>
+                <a href="#">Accessories</a>
+            </div>           
+             <div>
+                <a href="#">Wireless Networks</a>
+            </div>
+            <div>
+                <a href="#">Gaming Monitors</a>
+            </div>            
+            <div>
+                <a href="#">Virtual Reality</a>
+            </div>
+        </section>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,8 +34,8 @@ export default function Home() {
         <InfoBar />
         <FurnitureProducts />
         <ElectronicProducts />
-        {/*  
         <PopularSearchKeywords /> 
+        {/*  
         <Banner />
         <NewArrivals />
         <Brands /> */}
