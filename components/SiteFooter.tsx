bc87963de437c7452ae70e17716dbf1e061a0788
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-02-26 20:49:44 +0200

    Add SiteFooter component markup

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,6 +1,218 @@
 export default function SiteFooter()
 {
     return (
-        <footer></footer>
+        <footer>
+            <div>
+                <h3>Subscribe and get 20% discount.</h3>
+                <form>
+                    <input type="email" placeholder="Your email address"/>
+                    <input type="submit" value="Subscribe"/>
+                </form>
+            </div>
+            <div>
+                <div>
+                    <h5>Contact Us</h5>
+                    <div>
+                        <img src="./images/phone-call.svg" />
+                        <div>
+                            <strong>Monday-Friday: 08am-9pm</strong>
+                            +(1) 123 456 7890
+                        </div>
+                    </div>
+                    <div>
+                        <img src="./images/email.svg" />
+                        <div>
+                            <strong>Need help with your order?</strong>
+                            support@zeomart.com
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h5>About Zenmart</h5>
+                    <div>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    Track Your Order
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Product Guides
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Wishlists
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    Privacy Policy
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                Store Locator
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h5>Customer Support</h5>
+                    <div>
+                        <ul>
+                            <li>
+                                <a href="#">Contact Us</a>
+                            </li>
+                            <li>
+                                <a href="#">Help Centre</a>
+                            </li>
+                            <li>
+                                <a href="#">Returns & Exchanges</a>
+                            </li>
+                            <li>
+                                <a href="#">Best Buy Financing</a>
+                            </li>
+                            <li>
+                                <a href="#">Best Buy Gift Card</a>
+                            </li>                            
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h5>Services</h5>
+                    <div>
+                        <ul>
+                            <li>
+                                <a href="#">Geek Squad</a>
+                            </li>
+                            <li>
+                                <a href="#">In-Home Advisor</a>
+                            </li>
+                            <li>
+                                <a href="#">Trade-In Program</a>
+                            </li>                          
+                            <li>
+                                <a href="#">Electronics Recycling</a>
+                            </li>     
+                            <li>
+                                <a href="#">Best Buy Health</a>
+                            </li>     
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h5>Follow us</h5>
+                        <div>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <img src="./images/facebook.svg" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="./images/twitter.svg" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="./images/imstagram.svg" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="./images/linkedin.svg" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <h5>Mobile Apps</h5>
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <img src="./images/apple.svg" /> iOS App
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="./images/android.svg" /> Android App
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>We accept</h5>
+                        <div>
+                            <a href="#">
+                                <img src="./images/visa.svg" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                <img src="./images/mastercard.svg" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                <img src="./images/applepay.svg" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                <img src="./images/discover.svg" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                <img src="./images/paypal.svg" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="#">
+                                <img src="./images/amex.svg" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                © 2022 Zeomart. All Rights Reserved
+                <ul>
+                    <li>
+                        <a href="#">Privacy</a>
+                    </li>
+                    <li>
+                        <a href="#">Terms</a>
+                    </li>
+                    <li>
+                        <a href="#">Sitemap</a>
+                    </li>
+                </ul>
+                <div>
+                    <div>
+                        <a href="#">
+                            Currency USD
+                            <span>
+                                <img src="./images/arrow.svg" />
+                            </span>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            Language English
+                            <span>
+                                <img src="./images/arrow.svg" />
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
