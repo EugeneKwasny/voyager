f64449bacd4da97e99a1d593ae2c9517c2d78e65
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-02-26 09:54:54 +0200

    Add Hero cmponent markup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,44 @@
 export default function Hero()
 {
     return (
-        <section></section>
+        <section>
+            <div>
+                <img src="./images/hero-bg.png" />
+                <div>
+                    <div>Limited Edition</div>
+                    <div>
+                        Sport Essentials 
+                        <strong>Yoga Mats, Weights & more</strong>
+                    </div>
+                    <div>Discover our new items. Up to 25% Off !</div>
+                    <div>
+                        <a href="#">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div><img src="./images/arrow-right.svg" /></div>
+                <div><img src="./images/arrow-right.svg" /></div>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">4</a>
+                    </li>
+                    <li>
+                        <a href="#">5</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
     )
 }
