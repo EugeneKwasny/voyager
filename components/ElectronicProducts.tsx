a7299e372a837440ef5d36e616869f9eaa503bcd
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-02-26 20:06:50 +0200

    Add ElectronicProducts component markup

--- components/ElectronicProducts.tsx
+++ components/ElectronicProducts.tsx
@@ -1,6 +1,38 @@
+import ProductItem from "./ProductItem"
 export default function ElectronicProducts()
 {
     return (
-        <section></section>
+        <section>
+            <div>
+                <h3>Electronics products</h3>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="#">New arrivals</a>
+                        </li>
+                        <li>
+                            <a href="#">Best sellers</a>
+                        </li>
+                        <li>
+                            <a href="#">Best rated</a>
+                        </li>
+                        <li>
+                            <a href="#">All</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+            </div>
+            <nav>
+                <div><a href="#"><img src="./images/next.svg" /></a></div>
+                <div><a href="#"><img src="./images/next.svg" /></a></div>
+            </nav>
+        </section>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home() {
         <BestSellers />
         <InfoBar />
         <FurnitureProducts />
-        {/*  
         <ElectronicProducts />
+        {/*  
         <PopularSearchKeywords /> 
         <Banner />
         <NewArrivals />
