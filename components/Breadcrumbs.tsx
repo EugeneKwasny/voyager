f16dcc068b68c42023e5cac75af844b25bdffa82
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-02-27 10:56:58 +0200

    Add Breadcrumbs component markup

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -1,6 +1,21 @@
 export default function Breadcrumbs()
 {
     return (
-        <></>
+        <section>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Electronics</a>
+                </li>
+                <li>
+                    <a href="#">Computers</a>
+                </li>
+                <li>
+                    <a href="#">Desktop Computers</a>
+                </li>
+            </ul>
+        </section>
     )
 }
