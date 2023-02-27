5ca742bec71a7a30fddac5be94bff2833bf2aac2
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2023-02-27 10:55:22 +0200

    Add CategorySlider markup

--- components/CategorySlider.tsx
+++ components/CategorySlider.tsx
@@ -1,6 +1,42 @@
 export default function CategorySlider()
 {
     return(
-        <></>
+        <section>
+            <ul>
+                <li>
+                    <a href="#">All Electronics </a>
+                </li>
+                <li>
+                    <a href="#"> Smart TV </a>
+                </li>
+                <li>
+                    <a href="#">Laptops</a>
+                </li>
+                <li>
+                    <a href="#">Cell Phones</a>
+                </li>
+                <li>
+                    <a href="#">Camera & Photo </a>
+                </li>
+                <li>
+                    <a href="#">Portable Audio </a>
+                </li>
+                <li>
+                    <a href="#">Computers</a>
+                </li>
+                <li>
+                    <a href="#">iPad & Tablets </a>
+                </li>
+                <li>
+                    <a href="#">Pc Gaming </a>
+                </li>
+                <li>
+                    <a href="#">Smart Home</a>
+                </li>
+                <li>
+                    <a href="#">Headphones</a>
+                </li>
+            </ul>
+        </section>
     )
 }
